434d4427fd1d4617e4d2eb251c7cce08db178d07
Emmanuel Raju <emmanuel@able.do>
2022-03-02 18:56:35 +0530

    second commit

--- tree.js
+++ tree.js
@@ -16,33 +16,113 @@ const sampleInput = {
 	]
 }
 
-class node {
-    constructor(value){
+const assessmentInput = {
+    "name": "Lightning Cable",
+    "attributes": [
+        {
+            "name": "color",
+            "values": [
+                {
+                  "name": "red",
+                  "active": true
+                },
+                {
+                  "name": "blue",
+                  "active": true
+                },
+                {
+                  "name": "green",
+                  "active": true
+                }
+            ]
+        },
+        {
+            "name": "length",
+            "values": [
+                {
+                  "name": "1m",
+                  "active": true
+                },
+                {
+                  "name": "3m",
+                  "active": true
+                },
+                {
+                  "name": "6m",
+                  "active": false
+                }
+            ]
+        },
+        {
+            "name": "finish",
+            "values": [
+              {
+                "name": "nylon",
+                "active": true
+              },
+              {
+                "name": "rubber",
+                "active": false
+              }
+            ]
+        }
+    ]
+}
+
+class Node {
+    constructor(value,child){
         this.value = value
-        this.descendants = []
-        this.isRoot = false
-    }
-    set root(value){
-        this.isRoot = value
-    }
-    get root(){
-        return this.isRoot
+        this.children = child || []
     }
 }
 
 let nodes = []
+let helper = []
+let combinations = []
 
 let {name,attributes} = sampleInput
 
-// attributes.forEach(feature => feature.values.forEach( item => nodes.push(new node(item))))
-attributes[0].values.forEach( item => nodes.push(new node(item)))
-
-nodes.forEach(item => {
-    attributes[1].values.forEach( foo => item.descendants.push(new node(foo)))
-})
-// console.log("nodes des",nodes[1].descendants);
-
-for( i=0; i< nodes.length; i++){
-    nodes[i].descendants.forEach(item => attributes[2].values.forEach( foo => item.descendants.push(new node(foo))))
+for (i=attributes.length-1;i>=0;i--){
+    if (i==attributes.length-1){
+    attributes[i].values.forEach(value=>nodes.push(new Node(value)))
+    } else{
+        attributes[i].values.forEach(value=>{
+            helper.push(new Node(value,nodes))
+        })
+        nodes =helper
+        helper = []
+    }
 }
-console.log("nodes",nodes[1].descendants[0].descendants);
+
+const tree = new Node('root', nodes)
+
+console.log(tree);
+
+nodes.forEach(color=>{
+    // console.log(color.value)
+    color.children.forEach(length=>{
+        // console.log(color.value,length.value)
+        length.children.forEach(finish=>{
+            // console.log(color.value,length.value,finish.value)
+            combinations.push(`${color.value} | ${length.value} | ${finish.value}`)
+        })
+    })
+})
+
+console.log(combinations);
+
+// let nonSelectedNodes = [];
+//     let excludedSkus = [];
+//     let temp = [this];
+//     while (temp.length > 0) {
+//       let node = temp.pop();
+//       if (node.children.length === 0) {
+//         if (!node.isSelected) {
+//           nonSelectedNodes.push(node);
+//         }
+//         excludedSkus.push(...node.excludedSkus);
+//       } else {
+//         temp.push(...node.children);
+//       }
+//     }
+//     return { nonSelectedNodes: nonSelectedNodes, excludedSkus: excludedSkus };
